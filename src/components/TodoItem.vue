844e4ee82af43bf6b9d56825fee0472a4b7adf18
kanseii <kanseii@yahoo.com>
2019-01-08 15:14:57 +0800

    VueDemo

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,19 @@
+<template>
+    <li @click="handleDelete">{{content}}</li>
+</template>
+
+<script>
+export default {
+  props: ['content', 'index'],   // 声明使用 引用来自父组件的值
+  methods: {
+    handleDelete () {
+      this.$emit('delete', this.index)
+    }
+  }
+}
+
+</script>
+
+<style>
+
+</style>
